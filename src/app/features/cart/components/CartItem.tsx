b43d453afc890f2d7689c902a64e93f504c04f5f
Jordan Ferdinand <253198146+jordan-ferdinand_IMSI@users.noreply.github.com>
2026-04-04 12:32:00 +0700

    update: cart page

---
+++ src/app/features/cart/components/CartItem.tsx
@@ -0,0 +1,59 @@
+import { Minus, Plus, Trash2, Save } from "lucide-react";
+import { formatCurrency } from "@/app/lib/currency";
+import { Button } from "react-day-picker";
+
+export const CartItem = ({ item, handlers }: any) => {
+  const { handleUpdateQuantity, handleRemoveItem, handleSaveForLater } =
+    handlers;
+
+  return (
+    <div className="flex justify-between p-4 border rounded-lg">
+      <div>
+        <h3>{item.product.name}</h3>
+        <p>{formatCurrency(item.product.price)}</p>
+
+        <div className="flex gap-2 mt-2">
+          <button
+            onClick={() =>
+              handleUpdateQuantity(
+                item.product.id,
+                item.quantity - 1,
+                item.product.stock,
+              )
+            }
+          >
+            <Minus />
+          </button>
+
+          <span>{item.quantity}</span>
+
+          <button
+            onClick={() =>
+              handleUpdateQuantity(
+                item.product.id,
+                item.quantity + 1,
+                item.product.stock,
+              )
+            }
+          >
+            <Plus />
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <p>{formatCurrency(item.product.price * item.quantity)}</p>
+
+        <Button onClick={() => handleSaveForLater(item.product.id)}>
+          <Save />
+        </Button>
+
+        <Button
+          onClick={() => handleRemoveItem(item.product.id, item.product.name)}
+        >
+          <Trash2 />
+        </Button>
+      </div>
+    </div>
+  );
+};
