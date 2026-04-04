ab027ce0a5c7981a5578b468ed8674d31c8d8a7a
Jordan Ferdinand <253198146+jordan-ferdinand_IMSI@users.noreply.github.com>
2026-04-04 12:33:17 +0700

    update: routes

---
+++ src/app/router/routes.tsx
@@ -0,0 +1,31 @@
+import { RouteObject } from "react-router-dom";
+import { Layout } from "../shared/layout/Layout";
+import { CartPage } from "../features/cart/index.page";
+import { CheckoutPage } from "../features/checkout/CheckoutPage";
+import { AboutPage } from "../features/marketing/AboutPage";
+import ContactPage from "../features/marketing/contact";
+import ProductDetailPage from "../features/products/product";
+import { ProductsPage } from "../features/products/ProductsPage";
+import { NotFoundPage } from "../shared/layout/NotFoundPage";
+import { OrderConfirmationPage } from "../shared/layout/OrderConfirmationPage";
+
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Layout children />,
+    children: [
+      { index: true, element: <ProductsPage /> },
+
+      { path: "product/:id", element: <ProductDetailPage /> },
+      { path: "cart", element: <CartPage /> },
+      { path: "checkout", element: <CheckoutPage /> },
+      { path: "order-confirmation/:id", element: <OrderConfirmationPage /> },
+
+      { path: "about", element: <AboutPage /> },
+      { path: "contact", element: <ContactPage /> },
+
+      { path: "*", element: <NotFoundPage /> },
+    ],
+  },
+];
